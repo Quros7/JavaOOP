2ac746d9e74615b07162513a83ca5a566848ba7e
Quros7 <loparev.official@yandex.ru>
2023-09-24 23:37:05 +0500

    Request/response classes

--- ru/MoVe/SSK_bot/console/BotResponse.java
+++ ru/MoVe/SSK_bot/console/BotResponse.java
@@ -1,4 +1,10 @@
 package ru.MoVe.SSK_bot.console;
 
-public class BotResponse {
+import ru.MoVe.SSK_bot.AnswerWriter;
+
+public class BotResponse implements AnswerWriter {
+    String responseText;
+    public void writeAnswer(BotResponse response) {
+        System.out.println(response.responseText);
+    }
 }
