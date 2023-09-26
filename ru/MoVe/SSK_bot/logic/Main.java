f911b4fe8c66a1188c698608adcd11663c53b832
max <maximp2004@yandex.ru>
2023-09-26 10:50:37 +0500

    / instead !

--- ru/MoVe/SSK_bot/logic/Main.java
+++ ru/MoVe/SSK_bot/logic/Main.java
@@ -9,7 +9,7 @@ public class Main {
         msg = msg.getUserInput();
         Handler hl = new Handler();
         BotResponse rsp = new BotResponse();
-        while (!msg.getTextData().equals("!break")) {
+        while (!msg.getTextData().equals("/break")) {
             hl.handle(msg, rsp);
             msg = msg.getUserInput();
         }
