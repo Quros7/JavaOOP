d63d08169c81e16c13e247cc92006332125984b4
Quros7 <loparev.official@yandex.ru>
2023-10-17 00:26:38 +0500

    Refactored

--- src/main/java/ru/MoVe/SSK_bot/logic/Main.java
+++ src/main/java/ru/MoVe/SSK_bot/logic/Main.java
@@ -2,18 +2,30 @@ package ru.MoVe.SSK_bot.logic;
 
 import ru.MoVe.SSK_bot.console.Reader;
 import ru.MoVe.SSK_bot.console.Writer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
     public static void main (String[] args) {
-        Reader reader = new Reader();
-        Writer writer = new Writer();
-        Handler handler = new Handler();
-
-        BotRequest message = reader.getUserInput();
-
-        while (!message.isStopMessage()) {
-            handler.handle(message, writer);
-            message = reader.getUserInput();
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
+
+        // следующая часть не используется
+        // TODO: функционал бота на основе старых классов?
+//        Reader reader = new Reader();
+//        Writer writer = new Writer();
+//        Handler handler = new Handler();
+//
+//        BotRequest message = reader.getUserInput();
+//
+//        while (!message.isStopMessage()) {
+//            handler.handle(message, writer);
+//            message = reader.getUserInput();
+//        }
     }
 }
